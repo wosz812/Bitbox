726fcb3a78469d87a400b145e56cffc3453464a0
DESKTOP-TBJPPA3\bogus <9251992@naver.com>
2017-07-20 14:10:48 +0900

    Merge  해결

--- src/main/java/com/bitbox/service/SecurityService.java
+++ src/main/java/com/bitbox/service/SecurityService.java
@@ -1,6 +1,5 @@
 package com.bitbox.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -8,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class SecurityService implements PasswordEncoder{
 
-<<<<<<< HEAD
+
 	
 	
 	private ShaPasswordEncoder passwordEncoder;
@@ -45,41 +44,7 @@ public class SecurityService implements PasswordEncoder{
 	
 	
 }
-=======
-   
-   
-   private ShaPasswordEncoder passwordEncoder;
-   
-   private Object salt = null;
-   
-   public SecurityService(){
-      passwordEncoder = new ShaPasswordEncoder();
-   }
-   
-   public SecurityService(int sha){
-      passwordEncoder = new ShaPasswordEncoder(sha);
-   }
-   
-   public void setEncodeHashAsBase64(boolean encodeHashAsBase64) { 
-      passwordEncoder.setEncodeHashAsBase64(encodeHashAsBase64); 
-   } 
 
-   public void setSalt(Object salt){
-      this.salt = salt;
-   }
+  
    
-   @Override
-   public String encode(CharSequence rawPw) {
-      // TODO Auto-generated method stub
-      return passwordEncoder.encodePassword(rawPw.toString(), salt);
-   }
 
-   @Override
-   public boolean matches(CharSequence rawPw, String encodedPw) {
-      // TODO Auto-generated method stub
-      return false;
-   }
-   
-   
-}
->>>>>>> origin/master
