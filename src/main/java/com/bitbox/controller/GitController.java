31bfc4061cd3f2fc78de81e1335d5939c96e0665
DESKTOP-TBJPPA3\bogus <9251992@naver.com>
2017-07-24 14:11:30 +0900

    깃허브 로그인 바로접근하게 수정

--- src/main/java/com/bitbox/controller/GitController.java
+++ src/main/java/com/bitbox/controller/GitController.java
@@ -1,5 +1,7 @@
 package com.bitbox.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,23 +23,26 @@ public class GitController {
 	@Autowired
 	private OAuth2RestTemplate oauth2RestTemplate;
 	@RequestMapping(value = "/gitBoard", method = RequestMethod.GET)
-	public String gitBoard(@RequestParam("title") String title) {
+	public String gitBoard(@RequestParam("title") String title,HttpSession session) {
 		System.out.println("gitBoard controller: " + title);
-		String url = "index";
+		session.setAttribute("title",title);
+		String url = "redirect:http://localhost:8080/login/github";
 		return url;
 	}
+	
 
 	@RequestMapping(value = "/gitBoardView", method = RequestMethod.GET)
-	public String gitBoardView(String title, Model model) {
+	public String gitBoardView(String title, Model model,HttpSession session) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if (authentication instanceof OAuth2Authentication) {
 			OAuth2AccessToken accessToken = oauth2RestTemplate.getAccessToken();
-			
+	
 			OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) ((OAuth2Authentication) authentication)
 					.getDetails();
 //			model.addAttribute("token", details.getTokenValue());
 			model.addAttribute("token", accessToken.getValue());
 		}
+		model.addAttribute("title",session.getAttribute("title"));
 		System.out.println("gitBoard controller: " + title);
 		String url = "bitbox/gitBoard";
 		model.addAttribute("username", SecurityContextHolder.getContext().getAuthentication().getName());
