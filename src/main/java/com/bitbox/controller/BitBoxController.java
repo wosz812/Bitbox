a1d004f226bdc7046b81263e134b93fda37a8ec8
DESKTOP-DOGVD1T\BIT <mejinwoo367@naver.com>
2017-08-17 10:35:38 +0900

    Revert "qna 다른사용자의 글을 수정 및 삭제가 가능하던점 수정"
    This reverts commit b752042e68c7c0e14089e708907965fa9a333b07.

--- src/main/java/com/bitbox/controller/BitBoxController.java
+++ src/main/java/com/bitbox/controller/BitBoxController.java
@@ -403,18 +403,13 @@ public class BitBoxController {
 	public String readQna(HttpSession session, Model model, @RequestParam("page") int page,
 			@RequestParam("q_seq") int q_seq) {
 		String url = "/bitbox/qnaView";
-		QnaDTO qna = service.detailQna(q_seq);
+		QnaDTO qnaList = service.detailQna(q_seq);
 		List<ReQnaDTO> replyList = service.getReplyList(q_seq);
 		String replyUser = service.getReplyUser((String) session.getAttribute("id"));
-		int user = 0;
-		if(qna.getS_id().equals((String) session.getAttribute("id"))){
-			user = 1;
-		}
 		model.addAttribute("replyList", replyList);
-		model.addAttribute("qna", qna);
+		model.addAttribute("qnaList", qnaList);
 		model.addAttribute("replyUser", replyUser);
 		model.addAttribute("page", page);
-		model.addAttribute("user", user);
 		return url;
 	}
 
